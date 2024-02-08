4b09953b8fabb977fae5d09ec919dcd35fc1a70c
wkravchyk <wojciech.kravchyk@gmail.com>
2024-02-08 11:08:44 +0100

    Dodanie danych przykladowych

--- R/findMaxZ_funkcja.R
+++ R/findMaxZ_funkcja.R
@@ -8,12 +8,13 @@
 #'
 #' @examples
 findMaxZ = function(dirPath, hgtTresh) {
-  lasList = list.files(path = dirPath, pattern = ".las$", full.names = T)
+  lasList = list.files(path = paste0(getwd(), dirPath), pattern = ".laz$", full.names = T)
 
   for (i in 1:length(lasList)) {
-    tempLas = lidR::readLAS(lasList[i], filter = "-drop_z_below 0 -keep_class 2 3 4 5")
+    tempLas = lidR::readLAS(lasList[1], filter = "-drop_z_below 0 -keep_class 2 3 4 5")
     tempDf = as.data.frame(tempLas@data)
     najwyzszeDrzewo = tempDf[which.max(tempDf$Z), ]
+    najwyzszeDrzewo = najwyzszeDrzewo[, c(1:3, 5:7, 10,14)]
     if (najwyzszeDrzewo$Z > hgtTresh) {
       name = paste(basename(lasList[i]),".shp", sep = "")
       sf::st_write(sf::st_as_sf(najwyzszeDrzewo, coords = c("X","Y"), crs = 2180), paste0(dirPath, name))
